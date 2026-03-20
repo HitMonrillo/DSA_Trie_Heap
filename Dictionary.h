136135550e12f783efa614a8514844dc1ab0e2cc
meadowjp <136203773+meadowjp@users.noreply.github.com>
2026-03-19 20:07:27 -0400

    Update loadDict return type

--- Dictionary.h
+++ Dictionary.h
@@ -7,7 +7,7 @@ public:
   Dictionary();
   ~Dictionary();
 
-  vector<tuple<string, int>> loadDictionary(string filename);
+  unordered_map<string, int> loadDictionary(string filename);
   bool isValidLength(string currWord);
   bool hasValidChars(string currWord);
   int valueGenerator(string currWord);
