1522441cf174c12f1dc0c4db25fb926895b5d3a3
meadowjp <136203773+meadowjp@users.noreply.github.com>
2026-03-22 16:31:20 -0400

    Update main.cpp
    canForm check was already present in dictionary.cpp so removed it from main. (The code Luisa had for it was better so I replaced mine with hers)

--- main.cpp
+++ main.cpp
@@ -19,110 +19,82 @@ int calculateScore(string word) {
     } return total;
 }
 
-bool canForm(string word, string playerLetters) {
-    int available[26] = {0};
-    int blanks = 0;
-    for (int i = 0; i < playerLetters.length(); i++) {
-        if (playerLetters[i] == '!') {
-            blanks++;
-        }
-        else {
-            available[toupper(playerLetters[i]) - 'A']++;
-        }
-    }
-        for (int i = 0; i <word.length(); i++) {
-            int index = toupper(word[i]) - 'A';
-            if (available[index] > 0) {
-                available[index]--;
-            }
-            else if (blanks > 0) {
-                blanks--;
-            }
-            else {
-                return false;
+int main() {
+    cout << "--------------------\n" << "Scrabble Word Finder\n" << "---------------------" << endl;
+
+    bool running = true;
+    while (running) {
+        string userLetters = "";
+        int userContinue;
+        vector<string> bestWords = {};
+        bool validWord = true;
+
+        cout << "Enter your letters (don't include spaces, use \"!\" to denote blank tiles, ex. \"AREIG!P\"): " << endl;
+        cin >> userLetters;
+
+        for (int i=0; i<userLetters.length(); i++) {                                            //Checking validity of input
+            if (!isalpha(userLetters[i]) && (userLetters[i] != '!')) {
+                validWord = false;
             }
         }
-        return true;
-    }
 
-
-    int main() {
-        cout << "--------------------\n" << "Scrabble Word Finder\n" << "---------------------" << endl;
-
-        bool running = true;
-        while (running) {
-            string userLetters = "";
-            int userContinue;
-            vector<string> bestWords = {};
-            bool validWord = true;
-
-            cout << "Enter your letters (don't include spaces, use \"!\" to denote blank tiles, ex. \"AREIG!P\"): " << endl;
+        while (userLetters.length() != 7 || !validWord) {
+            cout << "Invalid input, please enter 7 letters with no spaces, use \"!\" to denote blank tiles: " << endl;            //Printing error message & repeating prompt if input is invalid
             cin >> userLetters;
+            validWord = true;
 
-            for (int i=0; i<userLetters.length(); i++) {                                            //Checking validity of input
+            for (int i=0; i<userLetters.length(); i++) {
                 if (!isalpha(userLetters[i]) && (userLetters[i] != '!')) {
                     validWord = false;
                 }
             }
+        }
 
-            while (userLetters.length() != 7 || !validWord) {
-                cout << "Invalid input, please enter 7 letters with no spaces, use \"!\" to denote blank tiles: " << endl;            //Printing error message & repeating prompt if input is invalid
-                cin >> userLetters;
-                validWord = true;
+        Dictionary dict;
+        vector<string> dictionary = dict.loadDictionary("scrabbleDictionary.csv", userLetters);
 
-                for (int i=0; i<userLetters.length(); i++) {
-                    if (!isalpha(userLetters[i]) && (userLetters[i] != '!')) {
-                        validWord = false;
-                    }
-                }
+        auto startTrie = high_resolution_clock::now();                                            //Timing trie
+        //Function that runs trie here, store result in vector bestWords
+        auto stopTrie = high_resolution_clock::now();
+
+        auto startHeap = high_resolution_clock::now();                                            //Timing min heap
+        MinHeap minHeap;
+        for (int i =0; i <dictionary.size(); i++) {
+            string word = dictionary[i];
+            int score = calculateScore(word);
+            if (minHeap.size()< 10)
+                minHeap.insert(word, score);
+            else if (score > minHeap.peek().score){
+                minHeap.extractMin();
+                minHeap.insert(word, score);
             }
+        }
+        
+        auto stopHeap = high_resolution_clock::now();
 
-            Dictionary dict;
-            vector<string> dictionary = dict.loadDictionary("scrabbleDictionary.csv", userLetters);
+        cout << "10 Best Words: " << endl;                                                         //Printing results
+        minHeap.printHeap();
 
-            auto startTrie = high_resolution_clock::now();                                            //Timing trie
-            //Function that runs trie here, store result in vector bestWords
-            auto stopTrie = high_resolution_clock::now();
+        auto trieDuration = duration_cast<microseconds>(stopTrie-startTrie);                       //Printing runtimes
+        cout << "Trie Runtime: " << trieDuration.count() << "microseconds" << endl;
+        auto heapDuration = duration_cast<microseconds>(stopHeap-startHeap);
+        cout << "Min Heap Runtime: " << heapDuration.count() << "microseconds" << endl;
 
-            auto startHeap = high_resolution_clock::now();                                            //Timing min heap
-            MinHeap minHeap;
-            for (int i =0; i <dictionary.size(); i++) {
-                string word = dictionary[i];
-                if (canForm(word, userLetters)) {
-                    int score = calculateScore(word);
-                    if (minHeap.size()< 10)
-                        minHeap.insert(word, score);
-                    else if (score > minHeap.peek().score){
-                        minHeap.extractMin();
-                        minHeap.insert(word, score);
-                    }
-                }
-            }
-            auto stopHeap = high_resolution_clock::now();
+        cout << "\nDo you want to continue?" << endl;                                              //Printing continue menu
+        cout << "1. Try another set of letters" << endl;
+        cout << "2. Exit" << endl;
+        cin >> userContinue;
 
-            cout << "10 Best Words: " << endl;                                                         //Printing results
-            minHeap.printHeap();
-
-            auto trieDuration = duration_cast<microseconds>(stopTrie-startTrie);                       //Printing runtimes
-            cout << "Trie Runtime: " << trieDuration.count() << "microseconds" << endl;
-            auto heapDuration = duration_cast<microseconds>(stopHeap-startHeap);
-            cout << "Min Heap Runtime: " << heapDuration.count() << "microseconds" << endl;
-
-            cout << "\nDo you want to continue?" << endl;                                              //Printing continue menu
+        while (userContinue != 1 && userContinue != 2) {                                           //Printing error message & repeating prompt if input is invalid
+            cout << "Invalid selection, please try again." << endl;
             cout << "1. Try another set of letters" << endl;
             cout << "2. Exit" << endl;
             cin >> userContinue;
-
-            while (userContinue != 1 && userContinue != 2) {                                           //Printing error message & repeating prompt if input is invalid
-                cout << "Invalid selection, please try again." << endl;
-                cout << "1. Try another set of letters" << endl;
-                cout << "2. Exit" << endl;
-                cin >> userContinue;
-            }
-
-            if (userContinue == 2) {                                                                    //Exiting program if 2 is entered
-                running = false;
-            }
         }
-        return 0;
+
+        if (userContinue == 2) {                                                                    //Exiting program if 2 is entered
+            running = false;
+        }
     }
+    return 0;
+}
