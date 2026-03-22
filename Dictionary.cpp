d946017db00347f78858936b6bbe1615cace3726
meadowjp <136203773+meadowjp@users.noreply.github.com>
2026-03-22 16:15:15 -0400

    Update Dictionary.cpp

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,9 +1,59 @@
 #include "Dictionary.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+using namespace std;
 
-Dictionary::Dictionary() {
-  vector<string> validWords = {};
-}
+Dictionary::Dictionary(){}
 
 Dictionary::~Dictionary(){}
 
+bool isValidLength(string currWord) {                      //Weed out all words with length greater than 8
+  if (currWord.length() > 8) {
+    return false;
+  }
+  return true;
+}
 
+bool canForm(string word, string playerLetters) {
+    int available[26] = {0};
+    int blanks = 0;
+    for (int i = 0; i < playerLetters.length(); i++) {
+        if (playerLetters[i] == '!')
+            blanks++;
+        else
+            available[toupper(playerLetters[i]) - 'A']++;
+    }
+    for (int i = 0; i < word.length(); i++) {
+        int index = toupper(word[i]) - 'A';
+        if (available[index] > 0)
+            available[index]--;
+        else if (blanks > 0)
+            blanks--;
+        else
+            return false;
+    }
+    return true;
+}
+
+vector<string> Dictionary::loadDictionary (string filename, string userLetters) {
+  vector<string> editedDict = {};
+  
+  ifstream ogDict("scrabbleDictionary.csv");                  //Load dictionary file into ogDict
+  if (!ogDict.is_open()) {
+    cout << "Could not open dictionary file: " << filename << endl;
+    return editedDict;
+  }
+  
+  string currWord;    
+  while (getline(ogDict, currWord)) {                        //Read each word into currWord
+    if (isValidLength(currWord) && canForm(currWord, userLetters)) {                              //If currWord length and chars are valid,
+      editedDict.insert(currWord);                                                     //add to dict
+    }
+  }
+  
+  ogDict.close();
+  return editedDict;
+}
