c3d5cbb86d7279828ca9e862704e8e075dbfa667
meadowjp <136203773+meadowjp@users.noreply.github.com>
2026-03-22 16:07:11 -0400

    Update Dictionary.h

--- Dictionary.h
+++ Dictionary.h
@@ -2,10 +2,16 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
 class Dictionary {
 public:
   Dictionary();
   ~Dictionary();
+  vector<string> loadDictionary(string filename, string userLetters);
 };
 
 #endif DICTIONARY_H
