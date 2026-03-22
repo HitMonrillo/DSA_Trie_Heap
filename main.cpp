f60a65d3e0622034a310a052a4dfc9d69a69058b
Luisa <luisalmeida0106@gmail.com>
2026-03-22 14:39:41 -0400

    added minheap algorithm in main, fixed some bugs in main as well

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include <array>
 #include <chrono>
 #include <cctype>
+#include "MinHeap.h"
 using namespace std;
+using namespace std::chrono;
 //Helpers:
 int calculateScore(string word) {
     int scores[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -26,8 +28,9 @@ bool canForm(string word, string playerLetters) {
         else {
             available[toupper(playerLetters[i]) - 'A']++;
         }
+    }
         for (int i = 0; i <word.length(); i++) {
-            int index = toupper(word[i] - 'A');
+            int index = toupper(word[i]) - 'A';
             if (available[index] > 0) {
                 available[index]--;
             }
@@ -36,82 +39,89 @@ bool canForm(string word, string playerLetters) {
             }
             else {
                 return false;
-        }
-    }
-return true;
-}
-
-
-int main() {
-    cout << "--------------------\n" << "Scrabble Word Finder\n" << "---------------------" << endl;
-
-    bool running = true;
-    while (running) {
-        string userLetters = "";
-        int userContinue;
-        vector<string> bestWords = {};
-        bool validWord = true;
-        
-        cout << "Enter your letters (don't include spaces, use "!" to denote blank tiles, ex. "AREIG!P"): " << endl;
-        cin >> userLetters;
-
-        for (int i=0; i<userLetters.length(); i++) {                                            //Checking validity of input
-            if (!isalpha(userLetters[i]) && (userLetters[i] != '!')) {
-                validWord = false;
             }
         }
+        return true;
+    }
 
-        while (userLetters.length() != 7 || !validWord) {
-            cout << "Invalid input, please enter 7 letters with no spaces, use \"!\" to denote blank tiles: " << endl;            //Printing error message & repeating prompt if input is invalid
+
+    int main() {
+        cout << "--------------------\n" << "Scrabble Word Finder\n" << "---------------------" << endl;
+
+        bool running = true;
+        while (running) {
+            string userLetters = "";
+            int userContinue;
+            vector<string> bestWords = {};
+            bool validWord = true;
+
+            cout << "Enter your letters (don't include spaces, use \"!\" to denote blank tiles, ex. \"AREIG!P\"): " << endl;
             cin >> userLetters;
-            validWord = true;
-           
-            for (int i=0; i<userLetters.length(); i++) {
+
+            for (int i=0; i<userLetters.length(); i++) {                                            //Checking validity of input
                 if (!isalpha(userLetters[i]) && (userLetters[i] != '!')) {
                     validWord = false;
                 }
             }
-        }
 
-        //functions for reading in CSV and processing it will go here
-        
-        auto startTrie = high_resolution_clock::now();                                            //Timing trie
-        //Function that runs trie here, store result in vector bestWords
-        auto stopTrie = high_resolution_clock::now();
+            while (userLetters.length() != 7 || !validWord) {
+                cout << "Invalid input, please enter 7 letters with no spaces, use \"!\" to denote blank tiles: " << endl;            //Printing error message & repeating prompt if input is invalid
+                cin >> userLetters;
+                validWord = true;
 
-        auto startHeap = high_resolution_clock::now();                                            //Timing min heap
-        //Function that runs heap here
-        auto stopHeap = high_resolution_clock::now();
-        
-        cout << "10 Best Words: ";                                                                 //Printing results
-        for (int j=0; j<bestWords.size();j++) {
-            if (j==0) {
-                cout << bestWords[j];
-            } else {
-                cout << ", " << bestWords[j];
+                for (int i=0; i<userLetters.length(); i++) {
+                    if (!isalpha(userLetters[i]) && (userLetters[i] != '!')) {
+                        validWord = false;
+                    }
+                }
             }
-        }
 
-        auto trieDuration = duration_cast<microseconds>(stopTrie-startTrie);                       //Printing runtimes
-        cout << "Trie Runtime: " << trieDuration.count() << "microseconds" << endl;
-        auto heapDuration = duration_cast<microseconds>(stopHeap-startHeap);
-        cout << "Min Heap Runtime: " << heapDuration.count() << "microseconds" << endl;
+            //functions for reading in CSV and processing it will go here
 
-        cout << "\nDo you want to continue?" << endl;                                              //Printing continue menu
-        cout << "1. Try another set of letters" << endl;
-        cout << "2. Exit" << endl;
-        cin >> userContinue;
+            auto startTrie = high_resolution_clock::now();                                            //Timing trie
+            //Function that runs trie here, store result in vector bestWords
+            auto stopTrie = high_resolution_clock::now();
 
-        while (userContinue != 1 && userContinue != 2) {                                           //Printing error message & repeating prompt if input is invalid
-            cout << "Invalid selection, please try again." << endl;
+            auto startHeap = high_resolution_clock::now();                                            //Timing min heap
+            vector<string> dictionary = {}; // placeholder until Dictionary class is implemented
+            MinHeap minHeap;
+            for (int i =0; i <dictionary.size(); i++) {
+                string word = dictionary[i];
+                if (canForm(word, userLetters)) {
+                    int score = calculateScore(word);
+                    if (minHeap.size()< 10)
+                        minHeap.insert(word, score);
+                    else if (score > minHeap.peek().score){
+                        minHeap.extractMin();
+                        minHeap.insert(word, score);
+                    }
+                }
+            }
+            auto stopHeap = high_resolution_clock::now();
+
+            cout << "10 Best Words: " << endl;                                                         //Printing results
+            minHeap.printHeap();
+
+            auto trieDuration = duration_cast<microseconds>(stopTrie-startTrie);                       //Printing runtimes
+            cout << "Trie Runtime: " << trieDuration.count() << "microseconds" << endl;
+            auto heapDuration = duration_cast<microseconds>(stopHeap-startHeap);
+            cout << "Min Heap Runtime: " << heapDuration.count() << "microseconds" << endl;
+
+            cout << "\nDo you want to continue?" << endl;                                              //Printing continue menu
             cout << "1. Try another set of letters" << endl;
             cout << "2. Exit" << endl;
             cin >> userContinue;
-        }
 
-        if (userContinue == 2) {                                                                    //Exiting program if 2 is entered
-            running = false;
+            while (userContinue != 1 && userContinue != 2) {                                           //Printing error message & repeating prompt if input is invalid
+                cout << "Invalid selection, please try again." << endl;
+                cout << "1. Try another set of letters" << endl;
+                cout << "2. Exit" << endl;
+                cin >> userContinue;
+            }
+
+            if (userContinue == 2) {                                                                    //Exiting program if 2 is entered
+                running = false;
+            }
         }
-    }
         return 0;
-}
+    }
