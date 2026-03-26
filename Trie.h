6fcdf1d63f3a8856aadb9900093cbe1bc46c3714
HitMonrillo <59132883+HitMonrillo@users.noreply.github.com>
2026-03-25 22:31:06 -0400

    Added skeleton code to Trie.h and done some planning

--- Trie.h
+++ Trie.h
@@ -5,10 +5,27 @@
 #ifndef TRIE_H
 #define TRIE_H
 
+#include <array>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 
 
 class Trie {
+private:
+    Trie* root;
 
+    void Destroy(Trie* node);
+
+public:
+    Trie();
+    ~Trie();
+
+
+    void insert(const string& word);
+    bool contains(const string& word);
 };
 
 
