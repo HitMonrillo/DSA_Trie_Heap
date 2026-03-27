226c02095107bee964028a7c7fe109f2883a3909
HitMonrillo <59132883+HitMonrillo@users.noreply.github.com>
2026-03-27 10:02:43 -0400

    Added insert function, destroy now I have to work on the contains function

--- Trie.cpp
+++ Trie.cpp
@@ -3,8 +3,10 @@
 //
 
 #include "Trie.h"
+#include <cctype>
 #include <iostream>
 #include <string>
+#include <regex>
 using namespace std;
 
 TrieNode::TrieNode() : children{}, isEnd(false) {
@@ -13,8 +15,44 @@ TrieNode::TrieNode() : children{}, isEnd(false) {
     }
 }
 
-void Trie::Destroy(Trie *node) {
+Trie::Trie() {
+    root = new TrieNode();
+}
+
+
+void Trie::Destroy(TrieNode *node) {
     if (node == nullptr) {
         return;
     }
+    for (int i =0; i< (int)node->children.size(); i++) {
+        if (node->children[i] != nullptr) {
+            Destroy(node->children[i]);
+            node->children[i] = nullptr;
+        }
+    }
+    delete node; //
+}
+
+void Trie::insert(const string &word) {
+    static const regex not_letter("[^A-Za-z]");
+    string cleaned = regex_replace(word, not_letter, "");
+
+    TrieNode* current = root;
+    for (int i = 0; i < cleaned.size(); i++) {
+        int index = toupper((unsigned char)cleaned[i]) - 'A';
+        if (current->children[index] == nullptr) {
+            current->children[index] = new TrieNode();
+        }
+        current = current->children[index];
+    }
+    current->isEnd = true;
+}
+
+bool Trie::contains(const string &word) {
+
+}
+
+Trie::~Trie() {
+    Destroy(root);
+    root = nullptr;
 }
